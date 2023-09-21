12cf60c82018c4f5671f2964673f1e8a6fdb2622
unknown <doniaskima344@gmail.com>
2023-09-21 14:29:34 +0100

    Feat::construction: 20-NestJS_Queueing_Systems_Redis_and_Bull_Integration

--- 20-Nest.js_File_Upload/src/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UploadModule } from './upload/upload.module';
-import { ConfigModule } from '@nestjs/config';
-
-@Module({
-  imports: [UploadModule, ConfigModule.forRoot({ isGlobal: true })],
-})
-export class AppModule {}
