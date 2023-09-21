ae1f90f24caee9b0817efbcba862bd60a0b2295e
unknown <doniaskima344@gmail.com>
2023-09-21 22:33:28 +0100

    feat: :test_tube: 11-Nest.js_Unit_Testing

--- 11-Test/src/modules/Shared/ExceptionFilters/forbidden.exception.ts
+++
@@ -1,8 +0,0 @@
-import { HttpException, HttpStatus } from '@nestjs/common';
-
-export class CustomForbiddenException extends HttpException {
-  constructor() {
-    // Call the super constructor with a custom message and the HTTP status code FORBIDDEN (403).
-    super('Access Forbidden', HttpStatus.FORBIDDEN);
-  }
-}
--- 11-Test/src/modules/Users/DTO/create-users.dto.ts
+++
@@ -1,5 +0,0 @@
-export class CreateUserDTO {
-  readonly _id: number;
-  readonly _name: string;
-  readonly _age: number;
-}
